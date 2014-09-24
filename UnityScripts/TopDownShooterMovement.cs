10f6f9c99d6a54602baf82642697bcc04f32bf54
MrSkyhenge <MrSkyhenge@users.noreply.github.com>
2014-09-24 17:59:49 -0500

    Update and rename Top-down Shooter Movement(C#) to TopDownShooterMovement.cs

--- UnityScripts/TopDownShooterMovement.cs
+++ UnityScripts/TopDownShooterMovement.cs
@@ -4,7 +4,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class Move : MonoBehaviour {
+public class TopDownShooterMovement : MonoBehaviour {
 
 	public float xSpeed = 10f;
 	public float zSpeed = 10f;
