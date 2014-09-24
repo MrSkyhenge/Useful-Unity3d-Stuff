905b77bd2a9989f8e4f1680715b3115562c89f26
MrSkyhenge <MrSkyhenge@users.noreply.github.com>
2014-09-24 17:58:31 -0500

    Update and rename Get Gameobject Speed(C#) to GetGameobjectSpeed.cs

--- UnityScripts/GetGameobjectSpeed.cs
+++ UnityScripts/GetGameobjectSpeed.cs
@@ -1,10 +1,10 @@
-// Get the current speed of the gameobject this script is attached to. Originally posted
-// at Unity Answers.
+// Get the current speed of the gameobject this script is attached to. You can use the vaule of curSpeed to
+// to trigger event or actions. Originally posted at Unity Answers.
 
 using UnityEngine;
 using System.Collections;
  
-public class GetSpeed : MonoBehaviour
+public class GetGameobjectSpeed : MonoBehaviour
 {
 	private Vector3 previousPosition;
 	private float curSpeed; //Current speed of gameobject
