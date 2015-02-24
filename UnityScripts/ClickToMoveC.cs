2102748fb43e6a2b7da74a4303fe6bacd6f7d64f
MrSkyhenge <MrSkyhenge@fakeemail.com>
2015-02-24 15:10:18 -0800

    Commit 2/24/2015

---
+++ UnityScripts/ClickToMoveC.cs
@@ -0,0 +1,29 @@
+﻿// Conversion of the Click To Move script from the Unity
+// Wiki converted to C#
+
+using UnityEngine;
+using System.Collections;
+
+public class ClickToMoveC : MonoBehaviour {
+ 
+	public int smooth; // Determines how quickly object moves towards position
+	private Vector3 targetPosition;
+ 
+	void  Update (){
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			var playerPlane= new Plane(Vector3.up, transform.position);
+			var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			float hitdist= 0.0f;
+	 
+			if (playerPlane.Raycast (ray, out hitdist)) {
+				var targetPoint= ray.GetPoint(hitdist);
+				targetPosition = ray.GetPoint(hitdist);
+				var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+				transform.rotation = targetRotation;
+			}
+		}
+	 
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * smooth);
+	}
+}
---
+++ UnityScripts/SimpleTurnBasedScript.cs
@@ -0,0 +1,44 @@
+﻿// This is a very, very simple example of ho to switch turns between a player and enemy.
+// Attack this script to a game object in your scene and run
+// Originally posted somewhere on Unity Answers and has been slightly modified
+
+using UnityEngine;
+using System.Collections;
+
+public class SimpleTurnBasedScript : MonoBehaviour{
+	bool displayGui = false;
+	
+	void Awake () {
+
+	}
+
+	void Start () {
+		Invoke("PlayerTurn", 1f); //Immediately start our loop
+	}
+
+	void Update () {
+		
+	}
+	
+	void PlayerTurn(){
+		Debug.Log ("Player Turn"); //Logs the player's turn to the console
+		displayGui = true;
+		//Insert your player-specific code here
+	}
+	
+	void EnemyTurn(){
+		Debug.Log ("Enemy Turn"); //Logs the enemy's turn to the console
+		displayGui = false;
+		// Enemy turn waits five seconds in this example. Change to enemy-specific code here
+		Invoke("PlayerTurn", 5f);  
+	}
+	
+	void OnGUI() {
+        if (!displayGui) return;
+		if (GUI.Button(new Rect(10, 10, 70, 50), "End Turn"))
+			Invoke("EnemyTurn", 0.1f);
+	}
+}
+
+
+
